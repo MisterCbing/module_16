23ebb36eabb3fbfab8f6c0e655281ce6091bafb7
MisterCbing <da@susanin.news>
2024-11-05 19:53:51 +0400

    v 1.1 - user.id стал int. - id присваивается как +1 к максимальному существующему id - из create_user убрано user: User, присвоение класса происходит внутри функции.

--- module_16_4.py
+++ module_16_4.py
@@ -6,7 +6,7 @@ app = FastAPI()
 users = []
 
 class User(BaseModel):
-    id: str
+    id: int
     username: str
     age: str
 
@@ -17,22 +17,24 @@ async def get_all_users():
 @app.get('/user/{user_id}')
 async def get_user(user_id: str):
     for i in users:
-        if i.id == user_id:
+        if str(i.id) == user_id:
             return i
     raise HTTPException(status_code=404, detail='User not found')
 
 @app.post('/user/{username}/{age}')
-async def create_user(user: User, username: str, age:str):
-    user.id = str(len(users) + 1)
-    user.username = username
-    user.age = age
+async def create_user(username: str, age:str):
+    if users:
+        user_id = (max(users, key=lambda x: int(x.id))).id + 1
+    else:
+        user_id = 1
+    user = User(id = user_id, username = username, age = age)
     users.append(user)
     return user
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: str, username:str, age: str):
     for i in users:
-        if i.id == user_id:
+        if str(i.id) == user_id:
             i.username = username
             i.age = age
             return i
@@ -41,7 +43,7 @@ async def update_user(user_id: str, username:str, age: str):
 @app.delete('/users/{user_id}')
 async def delete_user(user_id: str):
     for i in users:
-        if i.id == user_id:
+        if str(i.id) == user_id:
             users.remove(i)
             return i
     raise HTTPException(status_code=404, detail='User not found')
